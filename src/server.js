0600778f9aab0360e64af7fd88df10b78b500153
Hammad Danish <150535174+Hammadfudda@users.noreply.github.com>
2026-08-22 16:43:37 +0500

    updated

--- src/server.js
+++
@@ -1,8 +0,0 @@
-const app = require('./app');
-require('./jobs/purgeEmployees');
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT} in ${process.env.NODE_ENV || 'development'} mode`);
-});
